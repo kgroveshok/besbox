17d22a356348b3ee432da2653a8850a7b6258574
fluffy@besbox.com <fluffy@besbox.com>
2017-10-18 20:26:56 +0000

    updated

--- besbox.pl
+++ besbox.pl
@@ -1,6 +1,38 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl
 
 # besbox core
 
-# start menu system
+# TODO
+# start menu system nav
+# storage device control
+# futher menu features
+#
+#
+
+
+my $menu = {
+	"Storage" => {
+		"Select" => sub { print "Select device"  },
+		
+	},
+	"Music" => {
+		"Play" => sub { print "Play" },
+		"Next" => sub { print "Next" },
+	},
+	"Video" => {
+	}
+};
+
+my $menuItem="Storage";
+#while( 1 ) {
+	
+foreach my $i  ( keys $menu ) {
+	print $i;
+}
+
+
+#}
+
+
+
 
